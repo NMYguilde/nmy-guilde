584fcc3002590c416f2937db1fcfbfda50516ad3
unknown <ethtaylan@gmail.com>
2022-02-07 22:27:25 +0100

    dropdownmenu and JS

---
+++ index.js
@@ -0,0 +1,38 @@
+document.getElementById("pageLaGuilde");
+document.getElementById("menuLaGuilde");
+document.getElementById("msg");
+document.getElementById("menuAccueil");
+document.getElementById("menuRoster");
+document.getElementById("membresTeams")
+document.getElementById("dropDownTeams")
+document.getElementById("dropDownMembres")
+
+menuLaGuilde.onclick = function() {
+    pageLaGuilde.style.transform = "translate(0%)";
+    pageLaGuilde.style.opacity ="0.85";
+    msg.style.display = "none";
+}
+
+menuAccueil.onclick = function() {
+    pageLaGuilde.style.transform = "translate(-100%)"
+    pageLaGuilde.style.opacity ="0"
+    msg.style.display ="block"
+
+}
+
+menuRoster.onclick = function() {
+    membresTeams.style.opacity ="1";
+}
+
+dropDownTeams.onclick = function() {
+    membresTeams.style.opacity = "0";
+    pageLaGuilde.style.transform = "translate(-100%)"
+    pageLaGuilde.style.opacity ="0"
+}
+
+dropDownMembres.onclick = function() {
+    membresTeams.style.opacity = "0";
+    pageLaGuilde.style.transform = "translate(-100%)"
+    pageLaGuilde.style.opacity ="0"
+}
+
