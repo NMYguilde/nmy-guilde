84512b6e1f430e55f7c54c113a250b5223244754
unknown <ethtaylan@gmail.com>
2022-02-10 19:10:21 +0100

    mobile

--- index.js
+++ index.js
@@ -2,44 +2,43 @@ document.getElementById("pageLaGuilde");
 document.getElementById("menuLaGuilde");
 document.getElementById("msg");
 document.getElementById("menuAccueil");
-document.getElementById("menuRoster");
-document.getElementById("membresTeams")
-document.getElementById("dropDownTeams")
-document.getElementById("dropDownMembres")
+document.getElementById("menuDiscord");
+document.getElementById("discordDiv")
+document.getElementById("discordClose")
+
 
 menuLaGuilde.onclick = function() {
     pageLaGuilde.style.transform = "translate(0%)";
     pageLaGuilde.style.opacity ="0.85";
     msg.style.display = "none";
+    discordDiv.style.height = "0px";
+    discordDiv.style.opacity = "0";
 }
 
 menuAccueil.onclick = function() {
     pageLaGuilde.style.transform = "translate(-100%)"
-    pageLaGuilde.style.opacity ="0"
+    pageLaGuilde.style.opacity ="0";
     msg.style.display ="block"
+    discordDiv.style.height = "0px";
+    discordDiv.style.opacity = "0";
 
-}
-
-menuRoster.onclick = function() {
-    membresTeams.style.opacity ="1";
-}
-
-dropDownTeams.onclick = function() {
-    membresTeams.style.opacity = "0";
-    pageLaGuilde.style.transform = "translate(-100%)"
-    pageLaGuilde.style.opacity ="0"
-}
-
-dropDownMembres.onclick = function() {
-    document.getElementById("membresTeams")
-    membresTeams.style.opacity = "0";
-    pageLaGuilde.style.transform = "translate(-100%)"
-    pageLaGuilde.style.opacity ="0"
 }
 
 function showHide() {
     if (membresTeams.style.opacity == "0") {
-        membresTeams.style.opacity ="1"
+        membresTeams.style.opacity ="1";
     }
 
+}
+
+menuDiscord.onclick = function() {
+    discordDiv.style.height = "500px";
+    discordDiv.style.opacity = "1";
+    pageLaGuilde.style.transform = "translate(-100%)"
+    pageLaGuilde.style.opacity ="0";
+}
+
+discordClose.onclick = function() {
+    discordDiv.style.height = "0px";
+    discordDiv.style.opacity = "0";
 }
