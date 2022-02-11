ba2cedbcb5d5c47ee61e081f6df4483480caf14f
canserkanuren <canserkan.uren@gmail.com>
2022-02-11 14:02:45 +0100

    feat: init npm project

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -d "dist/" ] ; then
+    rm -rf dist/
+fi
+
+mkdir dist/
+mkdir dist/ressources
+
+cp -r ressources/ ./dist/ressources
+cp index.html ./dist/
+cp index.js ./dist/
+cp style.css ./dist/
+
+ls -al dist
