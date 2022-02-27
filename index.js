94582474d59b22e6282f2107affd23fa5c82e875
unknown <ethtaylan@gmail.com>
2022-02-27 12:16:31 +0100

    push

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let btncontact = document.getElementById("btnContact")
 let btncontacttwo = document.getElementById("btnContactTwo")
-
+let bd1 = document.getElementById("bd1")
+let bd2 = document.getElementById("bd2")
 let boxcontact = document.getElementById("boxContact")
 let btnclose = document.getElementById("close")
 
@@ -8,6 +9,9 @@ btncontact.onclick = function() {
     boxcontact.style.height = "300px"
     boxcontact.style.opacity = "1"
     boxcontact.style.marginTop = "40px"
+    bd1.style.height = "auto"
+    bd2.style.height = "auto"
+
 
 }
 
@@ -19,4 +23,10 @@ btncontacttwo.onclick = function() {
 btnclose.onclick = function() {
     boxcontact.style.opacity = "0"
     boxcontact.style.height = "0.1px"
+    bd1.style.height = "100%"
+    bd2.style.height = "100%"
+
 }
+
+
+
